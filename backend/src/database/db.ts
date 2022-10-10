faa673556912cf2527c15cd3f337fbccb11b78cc
Lakanavarapu Naga Manikanta Anudeep <61861542+anudeep586@users.noreply.github.com>
2022-10-10 08:22:09 +0530

    Delete backend directory

--- backend/src/database/db.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-import configs from '../database/database.connection';
-
-const config = configs[process.env.NODE_ENV || 'development'];
-
-const db = knex(config);
-
-export default db;
